03fed1964185473e1d8d536bf0f1d9b3bf5ff1ad
yannick <yannickweber95@gmail.com>
2024-12-29 16:39:12 +0100

    crossfade now correctly works

--- app/src/main/java/com/acidsepp/rain/MainActivity.kt
+++ app/src/main/java/com/acidsepp/rain/MainActivity.kt
@@ -6,22 +6,120 @@ import android.media.MediaPlayer
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.DefaultAlpha
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.floatPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.LifecycleCoroutineScope
 import androidx.lifecycle.lifecycleScope
+import com.acidsepp.rain.ui.theme.RainTheme
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import java.time.Duration
 import java.util.concurrent.atomic.AtomicBoolean
+
+val rainLength: Duration = Duration.ofSeconds(16)
+val crossfadeLength: Duration = Duration.ofSeconds(1)
+val loopLength = rainLength.minus(crossfadeLength)
 
 class MainActivity : ComponentActivity() {
 
+    private var playerBCoroutineScope: Job? = null
+    private var playerACoroutineScope: Job? = null
+
     private lateinit var mediaPlayerA: MediaPlayer
     private lateinit var mediaPlayerB: MediaPlayer
-    private val stopped = AtomicBoolean(false)
+    private val dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+    private val volumePreferenceKey = floatPreferencesKey("volume")
+    private val stopped = AtomicBoolean(true)
 
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        mediaPlayerA = MediaPlayer.create(this, R.raw.raincrossfaded)
+        val volumePreference = runBlocking {
+            dataStore.data.map { preferences -> preferences[volumePreferenceKey] ?: 1.0f }.firstOrNull()
+        } ?: 1.0f
+
+        enableEdgeToEdge()
+        setContent {
+            RainTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { _ ->
+                    ImageWithBackground(
+                        painter = painterResource(id = R.drawable.background),
+                        backgroundDrawableResId = R.drawable.background,
+                        contentDescription = "",
+                        modifier = Modifier.fillMaxSize()
+                    )
+                    var sliderValue by remember { mutableFloatStateOf(volumePreference) }
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        PlayButton(lifecycleScope)
+                        Slider(
+                            value = sliderValue,
+                            onValueChange = {
+                                sliderValue = it
+                                mediaPlayerA.setVolume(it, it)
+                                mediaPlayerB.setVolume(it, it)
+                                lifecycleScope.launch {
+                                    dataStore.edit { settings ->
+                                        settings[volumePreferenceKey] = it
+                                    }
+                                }
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth(0.8f)
+                                .alpha(0.8f)
+                        )
+                    }
+                }
+            }
+        }
+
+        mediaPlayerA = MediaPlayer.create(this, R.raw.rain)
         mediaPlayerA.apply {
             setAudioAttributes(
                 AudioAttributes.Builder()
@@ -31,7 +129,7 @@ class MainActivity : ComponentActivity() {
             )
         }
 
-        mediaPlayerB = MediaPlayer.create(this, R.raw.raincrossfaded)
+        mediaPlayerB = MediaPlayer.create(this, R.raw.rain)
         mediaPlayerB.apply {
             setAudioAttributes(
                 AudioAttributes.Builder()
@@ -41,30 +139,49 @@ class MainActivity : ComponentActivity() {
             )
         }
 
-        lifecycleScope.launch {
+        startLooping(lifecycleScope)
+    }
+
+    private fun startLooping(lifecycleScope: LifecycleCoroutineScope) {
+        if (!stopped.get()) {
+            return
+        }
+        stopped.set(false)
+
+        playerACoroutineScope?.cancel()
+        playerBCoroutineScope?.cancel()
+
+        playerACoroutineScope = lifecycleScope.launch {
             while (!stopped.get()) {
+                mediaPlayerA.restart()
                 Log.i("", "Media Player A started")
-                mediaPlayerA.start()
-                delay(mediaPlayerA.duration.toLong() - 1000L)
-                delay(mediaPlayerA.duration.toLong() - 1000L)
+                delay(loopLength.toMillis() * 2)
             }
         }
 
-        lifecycleScope.launch {
+        playerBCoroutineScope = lifecycleScope.launch {
             while (!stopped.get()) {
-                delay(mediaPlayerA.duration.toLong() - 1000L)
+                delay(loopLength.toMillis())
+                mediaPlayerB.restart()
                 Log.i("", "Media Player B started")
-                mediaPlayerB.start()
-                delay(mediaPlayerA.duration.toLong() - 1000L)
+                delay(loopLength.toMillis())
             }
         }
     }
 
+    private fun stopLooping() {
+        if (stopped.get()) {
+            return
+        }
+        stopped.set(true)
+        mediaPlayerA.pause()
+        mediaPlayerB.pause()
+        playerACoroutineScope?.cancel()
+        playerBCoroutineScope?.cancel()
+    }
+
     override fun onDestroy() {
         super.onDestroy()
-        stopped.set(true)
-
-        // Release the MediaPlayer resources when the activity is destroyed
         mediaPlayerA.apply {
             stop()
             release()
@@ -73,8 +190,78 @@ class MainActivity : ComponentActivity() {
         mediaPlayerA.apply {
             stop()
             release()
+        }
+    }
+
+    @Composable
+    fun PlayButton(lifecycleScope: LifecycleCoroutineScope) {
+        var isPlaying by remember { mutableStateOf(true) }
+
+        // Main button logic
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(0.8f)
+                .height(200.dp)
+                .clickable {
+                    if (isPlaying) {
+                        stopLooping()
+                        isPlaying = false
+                    } else {
+                        startLooping(lifecycleScope)
+                        isPlaying = true
+                    }
+                },
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                imageVector = if (isPlaying) Icons.Filled.Clear else Icons.Filled.PlayArrow,
+                contentDescription = if (isPlaying) "Stop" else "Play",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .alpha(0.8f)
+            )
         }
     }
 }
 
 
+
+@Composable
+fun ImageWithBackground(
+    painter: Painter,
+    @DrawableRes backgroundDrawableResId: Int,
+    contentDescription: String?,
+    modifier: Modifier = Modifier,
+    alignment: Alignment = Alignment.Center,
+    contentScale: ContentScale = ContentScale.Crop,
+    alpha: Float = DefaultAlpha,
+    colorFilter: ColorFilter? = null
+) {
+    Box(
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(backgroundDrawableResId),
+            contentDescription = null,
+            modifier = Modifier
+                .matchParentSize()
+        )
+        Image(
+            painter = painter,
+            contentDescription = contentDescription,
+            alignment = alignment,
+            contentScale = contentScale,
+            alpha = alpha,
+            colorFilter = colorFilter,
+            modifier = Modifier
+                .matchParentSize()
+        )
+    }
+}
+
+fun MediaPlayer.restart() = try {
+    this.seekTo(0)
+    this.start()
+} catch (e: Exception) {
+    Log.i("", "Could not stop media player", e)
+}
