8ffcd85701878ab5257e565f8f3e3b4f65a4d8cd
yannick <yannickweber95@gmail.com>
2024-12-30 13:41:38 +0100

    version 0.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 34
         targetSdk = 34
         versionCode = 1
-        versionName = "0.7"
+        versionName = "0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
